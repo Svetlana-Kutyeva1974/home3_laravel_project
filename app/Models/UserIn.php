074485efe571ac78c0e18ea161faacdc75e9d2ad
Svetlana Kutyova <kutyovas@yandex.ru>
2022-09-29 18:08:00 +0300

    without verify

--- app/Models/UserIn.php
+++ app/Models/UserIn.php
@@ -4,34 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class UserIn extends Model
 {
     use HasFactory;
-    protected $table = 'user';
-
     protected $fillable = [
         'name',
         'email',
         'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
     ];
 }
